2242a5bf7793fc61ec0f4f5acf22210ff2ddafc4
mohammad-jf <m.h.jalifathi@gmail.com>
2024-09-18 00:39:07 +0330

    add property

---
+++ actions/addProperty.js
@@ -0,0 +1,58 @@
+"use server";
+import Property from "@/models/Property";
+import connectDB from "@/utils/connectDB";
+import getSession from "@/utils/getSession";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const addProperty = async (formData) => {
+  await connectDB();
+  const { userId } = await getSession();
+
+  if (!userId) {
+    throw new Error("user id is required");
+  }
+
+  // access all values from amenities and images
+  const amenities = formData.getAll("amenities");
+  const images = formData
+    .getAll("images")
+    .filter((image) => image.name !== "")
+    .map((image) => image.name);
+
+  const propertyData = {
+    owner: userId,
+    type: formData.get("type"),
+    name: formData.get("name"),
+    description: formData.get("description"),
+    location: {
+      street: formData.get("location.street"),
+      city: formData.get("location.city"),
+      state: formData.get("location.state"),
+      zipcode: formData.get("location.zipcode"),
+    },
+    beds: formData.get("beds"),
+    baths: formData.get("baths"),
+    square_feet: formData.get("square_feet"),
+    amenities,
+    rates: {
+      nightly: formData.get("rates.nightly"),
+      weekly: formData.get("rates.weekly"),
+      monthly: formData.get("rates.monthly"),
+    },
+
+    seller_info: {
+      name: formData.get("seller-info.name"),
+      email: formData.get("seller-info.email"),
+      phone: formData.get("seller-info.phone"),
+    },
+    images,
+  };
+
+  const newProperty = new Property(propertyData);
+  await newProperty.save();
+  revalidatePath("/", "layout");
+  redirect(`/properties/${newProperty._id}`);
+};
+
+export default addProperty;
