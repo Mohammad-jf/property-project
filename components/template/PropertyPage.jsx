fffd7bc2d9989049204d2e4d5820ab3f14425419
mohammad-jf <m.h.jalifathi@gmail.com>
2024-09-23 21:48:26 +0330

    property aside section

--- components/template/PropertyPage.jsx
+++ components/template/PropertyPage.jsx
@@ -3,6 +3,9 @@ import PropertyDetails from "../modules/PropertyDetails";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
 import PropertyImages from "../modules/PropertyImages";
+import BookmarkButton from "../modules/BookmarkButton";
+import ShareButtons from "../modules/ShareButtons";
+import PropertyContactForm from "../modules/PropertyContactForm";
 
 const PropertyPage = ({ property }) => {
   return (
@@ -24,6 +27,11 @@ const PropertyPage = ({ property }) => {
         <div className="container m-auto py-10 px-6">
           <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
             <PropertyDetails property={property} />
+            <aside className="space-y-4">
+              <BookmarkButton property={property} />
+              <ShareButtons property={property} />
+              <PropertyContactForm property={property} />
+            </aside>
           </div>
         </div>
       </section>
