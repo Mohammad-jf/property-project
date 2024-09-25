9dbce101ea772ca019f5df912aeb79728eb7de94
mohammad-jf <m.h.jalifathi@gmail.com>
2024-09-26 01:59:09 +0330

    search result ui change

--- components/template/SearchResultsPage.jsx
+++ components/template/SearchResultsPage.jsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
+import PropertyCard from "../modules/PropertyCard";
 import PropertySearchForm from "../modules/PropertySearchForm";
-import PropertiesPage from "./PropertiesPage";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
 
 const SearchResultsPage = ({ properties }) => {
   return (
@@ -9,7 +11,29 @@ const SearchResultsPage = ({ properties }) => {
           <PropertySearchForm />
         </div>
       </section>
-      <PropertiesPage properties={properties} />
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          <Link
+            href="/properties"
+            className="flex items-center text-blue-500 hover:underline mb-3"
+          >
+            <FaArrowAltCircleLeft className="mr-2 mb-1" /> Back To Properties
+          </Link>
+          <h1 className="text-2xl mb-4">Search Results</h1>
+          {properties.length === 0 ? (
+            <p>No search results found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property) => (
+                <PropertyCard
+                  key={property._id}
+                  property={JSON.parse(JSON.stringify(property))}
+                />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
     </>
   );
 };
