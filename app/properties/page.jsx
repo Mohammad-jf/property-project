b84186ab0fd0188c4a39187950f4fc0b57fa2050
mohammad-jf <m.h.jalifathi@gmail.com>
2024-10-06 00:13:42 +0330

    pagination implementation

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -2,12 +2,23 @@ import PropertiesPage from "@/components/template/PropertiesPage";
 import Property from "@/models/Property";
 import connectDB from "@/utils/connectDB";
 
-const Properties = async () => {
+const Properties = async ({ searchParams }) => {
   await connectDB();
+  const { page = 1 } = searchParams;
+  const propertyPerPage = 9;
+  const skip = (page - 1) * propertyPerPage;
+  const total = await Property.countDocuments({}).lean();
   // .lean optimses query performance by returning plain js objects as long as its readonly
-  const properties = await Property.find({}).lean();
+  const properties = await Property.find({}).skip(skip).limit(propertyPerPage);
 
-  return <PropertiesPage properties={properties} />;
+  return (
+    <PropertiesPage
+      properties={properties}
+      page={parseInt(page)}
+      propertyPerPage={parseInt(propertyPerPage)}
+      total={total}
+    />
+  );
 };
 
 export default Properties;
--- components/template/PropertiesPage.jsx
+++ components/template/PropertiesPage.jsx
@@ -1,6 +1,8 @@
+import Pagination from "../modules/Pagination";
 import PropertyCard from "../modules/PropertyCard";
 
-const PropertiesPage = ({ properties }) => {
+const PropertiesPage = ({ properties, page, propertyPerPage, total }) => {
+  const showPagination = total > propertyPerPage;
   return (
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
@@ -13,6 +15,13 @@ const PropertiesPage = ({ properties }) => {
             ))}
           </div>
         )}
+        {showPagination ? (
+          <Pagination
+            page={page}
+            propertyPerPage={propertyPerPage}
+            total={total}
+          />
+        ) : null}
       </div>
     </section>
   );
