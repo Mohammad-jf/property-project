9106fdf017d3c9152e7a5e61235414eeae2f80f4
mohammad-jf <m.h.jalifathi@gmail.com>
2024-10-06 03:06:51 +0330

    image swiper

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,7 +5,7 @@ import NextAuthProvider from "@/providers/NextAuthProvider";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { GlobalProvider } from "@/context/GlobalContext";
-
+import "photoswipe/dist/photoswipe.css";
 export const metadata = {
   title: "Property Listing",
   keywords: "rental, property, realstate",
