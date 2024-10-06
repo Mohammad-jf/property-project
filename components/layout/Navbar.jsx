62bbd0de4fb21ad9a62c23aa8a21044288ab19e3
mohammad-jf <m.h.jalifathi@gmail.com>
2024-10-06 03:40:57 +0330

    some fix

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -238,13 +238,15 @@ const Navbar = () => {
               </Link>
             )}
             {!session && (
-              <button
-                className="flex items-center text-white
-             bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5"
-              >
-                <FaGoogle className="mr-2 text-white" />
-                <span>Login or Register</span>
-              </button>
+              <Link href="/signin">
+                <button
+                  className="flex items-center text-white
+                bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5"
+                >
+                  <FaGoogle className="mr-2 text-white" />
+                  <span>Login or Register</span>
+                </button>
+              </Link>
             )}
           </div>
         </div>
