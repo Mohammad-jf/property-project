86c62f489a329d8c6d35034a0f70f0445c77ef53
mohammad-jf <m.h.jalifathi@gmail.com>
2024-10-13 00:57:06 +0330

    increase timeout for add page

--- app/properties/add/page.jsx
+++ app/properties/add/page.jsx
@@ -2,6 +2,7 @@ import AddPage from "@/components/template/AddPage";
 import getSession from "@/utils/getSession";
 import { redirect } from "next/navigation";
 
+export const maxDuration = 30;
 const Add = async () => {
   const session = await getSession();
   if (!session || !session?.user) {
